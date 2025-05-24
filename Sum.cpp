8acddbeb3ad7ea05625461734b5a7dc0c8af8655
naitikdbansal <labsconnectbusiness@gmail.com>
2025-05-24 14:14:53 +0530

    Add files via upload

---
+++ Sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int a;
+int b;
+int s;
+cout<<"eneter number 1: ";
+cin>>a;
+cout<<"eneter number 2: ";
+cin>>b;
+s=a+b;
+cout<<"your sum is: "<<s;
+return 0;
+}
